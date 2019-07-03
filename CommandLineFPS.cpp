d2a3bc6f1cc6141070c775dbfe7750ff4ed5092b
French Toast <50852696+ToastBoye@users.noreply.github.com>
2019-07-03 22:43:28 +0000

    lonely af doom

--- CommandLineFPS.cpp
+++ CommandLineFPS.cpp
@@ -77,15 +77,15 @@ using namespace std;
 
 int nScreenWidth = 120;			// Console Screen Size X (columns)
 int nScreenHeight = 40;			// Console Screen Size Y (rows)
-int nMapWidth = 16;				// World Dimensions
-int nMapHeight = 16;
+int nMapWidth = 20;				// World Dimensions
+int nMapHeight = 20;
 
 float fPlayerX = 14.7f;			// Player Start Position
 float fPlayerY = 5.09f;
 float fPlayerA = 0.0f;			// Player Start Rotation
 float fFOV = 3.14159f / 4.0f;	// Field of View
 float fDepth = 16.0f;			// Maximum rendering distance
-float fSpeed = 5.0f;			// Walking Speed
+float fSpeed = 10.0f;			// Walking Speed
 
 int main()
 {
@@ -97,22 +97,22 @@ int main()
 
 	// Create Map of world space # = wall block, . = space
 	wstring map;
-	map += L"#########.......";
-	map += L"#...............";
-	map += L"#.......########";
-	map += L"#..............#";
-	map += L"#......##......#";
-	map += L"#......##......#";
-	map += L"#..............#";
-	map += L"###............#";
-	map += L"##.............#";
-	map += L"#......####..###";
-	map += L"#......#.......#";
-	map += L"#......#.......#";
-	map += L"#..............#";
-	map += L"#......#########";
-	map += L"#..............#";
-	map += L"################";
+	map += L"####################";
+	map += L"#..................#";
+	map += L"#...........########";
+	map += L"#..................#";
+	map += L"#......##..........#";
+	map += L"#......##..........#";
+	map += L"#..................#";
+	map += L"###................#";
+	map += L"##.................#";
+	map += L"#..........####..###";
+	map += L"#..........#.......#";
+	map += L"#..........#.......#";
+	map += L"#..................#";
+	map += L"#..........#########";
+	map += L"#...,.,............#";
+	map += L"####################";
 
 	auto tp1 = chrono::system_clock::now();
 	auto tp2 = chrono::system_clock::now();
