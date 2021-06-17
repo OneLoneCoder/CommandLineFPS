8105cf0900b91d0988c7e4de7cc6f231098e2ca4
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-06-17 18:50:24 -0400

    Ported to *nix

--- CommandLineFPS.cpp
+++ CommandLineFPS.cpp
@@ -64,6 +64,10 @@
 
 	Last Updated: 27/02/2017
 */
+/*
+    Ported to *nix by PQCraft
+    Use `g++ CommandLineFPS.cpp -o CommandLineFPS -lX11' to compile
+*/
 
 #include <iostream>
 #include <vector>
@@ -73,10 +77,17 @@
 using namespace std;
 
 #include <stdio.h>
-#include <Windows.h>
+//#include <windows.h>
+#include <math.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <termios.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <X11/Xlib.h>
 
-int nScreenWidth = 120;			// Console Screen Size X (columns)
-int nScreenHeight = 40;			// Console Screen Size Y (rows)
+int nScreenWidth;			// Console Screen Size X (columns)
+int nScreenHeight;			// Console Screen Size Y (rows)
 int nMapWidth = 16;				// World Dimensions
 int nMapHeight = 16;
 
@@ -87,13 +98,86 @@ float fFOV = 3.14159f / 4.0f;	// Field of View
 float fDepth = 16.0f;			// Maximum rendering distance
 float fSpeed = 5.0f;			// Walking Speed
 
+struct termios tu_term, tu_old_term;
+bool tu_textlock = false;
+
+int kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+    if (!initialized) {
+        struct termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+    int bytes;
+    ioctl(STDIN, FIONREAD, &bytes);
+    return bytes;
+}
+
+void tu_lockTerm() {
+    if (!tu_textlock) {
+        tcgetattr(0, &tu_term);
+        tcgetattr(0, &tu_old_term);
+        tu_term.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(0, TCSANOW, &tu_term);
+        tu_textlock = true;
+    }
+}
+
+void tu_unlockTerm() {
+    if (tu_textlock) {
+        tcsetattr(0, TCSANOW, &tu_old_term);
+        tu_textlock = false;
+    }
+}
+
+Display* g_pDisplay = XOpenDisplay(getenv("DISPLAY"));
+
+bool GetKeyState(KeySym keySym)
+{
+    if(g_pDisplay == NULL)
+    {
+        return false;
+    }
+ 
+    char szKey[32];
+    int iKeyCodeToFind = XKeysymToKeycode(g_pDisplay, keySym);
+ 
+    XQueryKeymap(g_pDisplay, szKey);
+ 
+    return szKey[iKeyCodeToFind / 8] & (1 << (iKeyCodeToFind % 8));
+}
+
+void cleanExit(int sig) {
+    (void)sig;
+    tu_unlockTerm();
+    FILE* ret = freopen(NULL, "w", stdout);
+    (void)ret;
+	fputs("\e[H\e[2J\e[3J", stdout);
+    exit(0);
+}
+
 int main()
 {
+    signal(SIGINT, cleanExit);
+    
+    tu_lockTerm();
+    
+    struct winsize wsize;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsize);
+
+    nScreenWidth = wsize.ws_col;
+    nScreenHeight = wsize.ws_row;
+
 	// Create Screen Buffer
 	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwBytesWritten = 0;
+
+	wchar_t *buf = new wchar_t[256];
+
+    setlocale(LC_CTYPE, "");
 
 	// Create Map of world space # = wall block, . = space
 	wstring map;
@@ -127,38 +211,37 @@ int main()
 		tp1 = tp2;
 		float fElapsedTime = elapsedTime.count();
 
+	    // Handle CCW Rotation
+	    if (GetKeyState((unsigned short)'A'))
+		    fPlayerA -= (fSpeed * 0.75f) * fElapsedTime;
 
-		// Handle CCW Rotation
-		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
-			fPlayerA -= (fSpeed * 0.75f) * fElapsedTime;
+	    // Handle CW Rotation
+	    if (GetKeyState((unsigned short)'D'))
+		    fPlayerA += (fSpeed * 0.75f) * fElapsedTime;
+	    
+	    // Handle Forwards movement & collision
+	    if (GetKeyState((unsigned short)'W'))
+	    {
+		    fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
+		    fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
+		    if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+		    {
+			    fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
+			    fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
+		    }			
+	    }
 
-		// Handle CW Rotation
-		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
-			fPlayerA += (fSpeed * 0.75f) * fElapsedTime;
-		
-		// Handle Forwards movement & collision
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
-		{
-			fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
-			fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
-			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
-			{
-				fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
-				fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
-			}			
-		}
-
-		// Handle backwards movement & collision
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
-		{
-			fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
-			fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
-			if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
-			{
-				fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
-				fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
-			}
-		}
+	    // Handle backwards movement & collision
+	    if (GetKeyState((unsigned short)'S'))
+	    {
+		    fPlayerX -= sinf(fPlayerA) * fSpeed * fElapsedTime;;
+		    fPlayerY -= cosf(fPlayerA) * fSpeed * fElapsedTime;;
+		    if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+		    {
+			    fPlayerX += sinf(fPlayerA) * fSpeed * fElapsedTime;;
+			    fPlayerY += cosf(fPlayerA) * fSpeed * fElapsedTime;;
+		    }
+	    }
 
 		for (int x = 0; x < nScreenWidth; x++)
 		{
@@ -264,7 +347,10 @@ int main()
 		}
 
 		// Display Stats
-		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f/fElapsedTime);
+		
+		swprintf(buf, 256, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f/fElapsedTime);
+
+		wcsncpy(screen, buf, wcslen(buf));
 
 		// Display Map
 		for (int nx = 0; nx < nMapWidth; nx++)
@@ -276,7 +362,14 @@ int main()
 
 		// Display Frame
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+		fputs("\e[H", stdout);
+		fflush(stdout);
+        FILE* ret = freopen(NULL, "w", stdout);
+        (void)ret;
+		fputws(screen, stdout);
+		fflush(stdout);
+        ret = freopen(NULL, "w", stdout);
+        (void)ret;
 	}
 
 	return 0;
